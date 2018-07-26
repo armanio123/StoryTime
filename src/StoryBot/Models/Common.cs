8f4b6b230fe000276f9b9f5358e319a3bd8011c7
Armando Aguirre <armanio123@outlook.com>
2018-07-25 21:34:59 -0700

    Added choice keys

--- src/StoryBot/Models/Common.cs
+++ src/StoryBot/Models/Common.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace StoryBot.Models
 {
@@ -9,22 +6,22 @@ namespace StoryBot.Models
     {
         public static class ChoiceKeyEquivalents
         {
-            static string[] numbers = { "1", "2", "3", "4", "5" };
-            static string[] letters = { "a", "b", "c", "d", "e" };
-            static string[] words = { "first", "second", "third", "fourth", "fifth" };
-
-            static Dictionary<string, int> dictionary = new Dictionary<string, int>()
+            private static Dictionary<string, int> dictionary = new Dictionary<string, int>()
             {
-                { "1", 0},{ "a", 0},{ "first", 0},{ "1st", 0},{ "one", 0},{ "option one", 0},{ "first option", 0},
-                { "2", 1},{ "b", 1},{ "second", 1},{ "2nd", 1},{ "two", 1},{ "option two", 1},{ "second option", 1},
-                { "3", 2},{ "c", 2},{ "third", 2},{ "3rd", 2},{ "three", 2},{ "option three", 2},{ "third option", 2},
-                { "4", 3},{ "d", 3},{ "fourth", 3},{ "4th", 3},{ "four", 3},{ "option four", 3},{ "fourth option", 3},
-                { "5", 4},{ "e", 4},{ "fifth", 4},{ "5th", 4},{ "five", 4},{ "option five", 4},{ "fifth option", 4},
+                { "1", 0},{ "2", 1},{ "3", 2},{ "4", 3},{ "5", 4},
+                { "one", 0},{ "two", 1},{ "three", 2},{ "four", 3},{ "five", 4},
+                { "option one", 0},{ "option two", 1},{ "option three", 2},{ "option four", 3},{ "option five", 4},
+                { "option 1", 0},{ "option 2", 1},{ "option 3", 2},{ "option 4", 3},{ "option 5", 4},
+                { "a", 0},{ "b", 1},{ "c", 2},{ "d", 3},{ "e", 4},
+                { "option a", 0},{ "option b", 1},{ "option c", 2},{ "option d", 3},{ "option e", 4},
+                { "first", 0},{ "second", 1},{ "third", 2},{ "fourth", 3},{ "fifth", 4},
+                { "1st", 0},{ "2nd", 1},{ "3rd", 2},{ "4th", 3},{ "5th", 4},
+                { "first option", 0},{ "second option", 1},{ "third option", 2},{ "fourth option", 3},{ "fifth option", 4}
             };
 
             public static int GetChoiceKeyMatch(string key)
             {
-                if(dictionary.ContainsKey(key))
+                if (dictionary.ContainsKey(key))
                 {
                     return dictionary[key];
                 }
